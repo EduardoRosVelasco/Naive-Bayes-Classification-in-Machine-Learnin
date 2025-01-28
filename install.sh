9a6ed8e0b398661f07023cd6567784f8ed6a14c1
EduardoRosVelasco <rosvelascoeduardo@gmail.com>
2025-01-28 09:44:26 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
-conda=/usr/local/anaconda3/bin/conda
-activate=/usr/local/anaconda3/bin/activate
-deactivate=/usr/local/anaconda3/bin/deactivate
+#!/bin/bash
 
-$conda create -y --no-default-packages -n ML_py37 python=3.7
-source $activate  ML_py37 
-pip install -r requirements.txt
-conda $deactivate
+# Instalar las dependencias necesarias para ejecutar el proyecto
 
+echo "Actualizando el sistema..."
+sudo apt update && sudo apt upgrade -y
 
+echo "Instalando Python y pip..."
+sudo apt install -y python3 python3-pip
 
+echo "Instalando las bibliotecas necesarias..."
+pip3 install pandas numpy matplotlib jupyter
 
+echo "Configuración completada. Puedes abrir el notebook ejecutando:"
+echo "jupyter notebook \"final bayes.ipynb\""
 
